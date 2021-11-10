b2b155d0db09e9a048c1b6c98c6d27e1582aac83
ihsan <ihsanfajar@refactory.id>
2021-11-10 09:30:50 +0700

    add '@' path resolver to 'src'

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import Layouts from 'vite-plugin-vue-layouts'
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
+      '@': path.resolve(__dirname, './src')
     }
   },
   plugins: [
